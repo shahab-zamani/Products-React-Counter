105f09736379d2b36850872a671f42ae6c5e5500
shahab.zamani <shahab.mirzamani@gmail.com>
2022-02-11 17:51:06 -0500

    index.js import app pas to root added

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import App from './app';
+import reactDOM from 'react-dom'; 
+import {render} from "@testing-library/react";
+
+// const element = <h1>Products Program</h1>
+reactDOM.render(<App />,document.getElementById('root'));
+
