903696a01c631f104f271f7581d8595483e4caba
shahab.zamani <shahab.mirzamani@gmail.com>
2022-02-11 17:50:09 -0500

    component app added

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import {Component} from 'react';
+
+class App extends Component{
+    render () {
+        return (
+            <>
+            <h1>Products Name1</h1>
+            <h1>Products Name2</h1>
+            </>
+        );
+    }
+}
+
+export default App;
+
