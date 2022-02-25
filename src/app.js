3ba4c1540a58dc8a459fcb9d3ae6bb5b7d19eb9f
shahab.zamani <shahab.mirzamani@gmail.com>
2022-02-25 11:09:59 -0500

    add app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 import {Component} from 'react';
 
-class App extends Component{
-    render () {
-        return (
-            <>
-            <h1>Products Name1</h1>
-            <h1>Products Name2</h1>
-            <h1>Products Name3</h1>
-            </>
-        );
-    }
+// class App extends Component{
+//     render () {
+//         return (
+//             <>
+//             <h1>Products Name1</h1>
+//             <h1>Products Name2</h1>
+//             <h1>Products Name3</h1>
+//             </>
+//         );
+//     }
+// }
+
+
+function App (){
+    return (
+                    <>
+                    <h1>Products Name1</h1>
+                    <h1>Products Name2</h1>
+                    <h1>Products Name3</h1>
+                    </>
+                );   
 }
+
 
 export default App;
 
