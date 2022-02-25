f02f45512f760dc51111b5312426ffb900b2d486
shahab.zamani <shahab.mirzamani@gmail.com>
2022-02-25 14:05:33 -0500

    add calculate sum to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import  'bootstrap/dist/css/bootstrap.min.css';
+import Products from './products';
 
 
 class Navbar extends Component {
     render() { 
         return ( 
-            <nav className='navbar navbar-bg navbar-light '>
-            <div className="container-fluid "></div>
-            <a href='#'   className='navbar-brand'> Navbar 5 </a>
+            <nav className='navbar navbar-bg navbar-light '>            
+
+            <div className="container-fluid"></div>
+            <a href='#'   className='navbar-brand'> Navbar {this.calculateSum()} </a>
             </nav>
         );
     }
+
+    calculateSum = () => {
+        let sum = 0;
+        this.props.products.forEach( p => {
+            sum += p.count ;            
+        })
+        return sum 
+    
+    }
 }
+
  
 export default Navbar;
