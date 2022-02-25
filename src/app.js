c54014dc0cd1628b27200f97d2a0fc264b1caae0
shahab.zamani <shahab.mirzamani@gmail.com>
2022-02-25 14:18:24 -0500

    add state and method to parent

--- src/app.js
+++ src/app.js
@@ -2,28 +2,82 @@ import {Component} from 'react';
 import Products from './components/products';
 import Navbar from './components/navbar';
 
-// class App extends Component{
-//     render () {
-//         return (
-//             <>
-//             <h1>Products Name1</h1>
-//             <h1>Products Name2</h1>
-//             <h1>Products Name3</h1>
-//             </>
-//         );
-//     }
-// }
+ class App extends Component{
 
 
-function App (){
-    return (
-                    <>
-                    <Navbar></Navbar>
-                    <Products></Products>
-                    </>
-                );   
+    state = {
+        products : [
+        {id: 1, count : 2 , productName : 'Laptop'},
+        {id: 2, count : 3 , productName : 'Phone'},
+        {id: 3, count : 5 , productName : 'Airpod'}
+                ]
+            } 
+
+
+    render () {
+       return (
+            <>
+             <Navbar products = {this.state.products}/>
+             <Products  
+             products = {this.state.products}
+             onDelete = {this.handleDelete} 
+             onIncrement = {this.handleIncrement} 
+             onDecrement = {this.handleDecrement} 
+             onReset = {this.handleReset} 
+             />
+             </>
+
+
+    );
+   }
+   handleIncrement = (productId) => {
+    const newProducts =[...this.state.products];
+    const index = newProducts.findIndex(p=> p.id === productId);
+    newProducts[index].count  += 1;
+    this.setState({Products : newProducts});
+
+
+}
+
+handleDecrement = (productId) => {
+  const newProducts =[...this.state.products];
+  const index = newProducts.findIndex(p=> p.id === productId);
+  newProducts[index].count  -= 1;
+  this.setState({products : newProducts});
+
+
 }
 
 
-export default App;
+
+
+
+handleDelete = (productId) =>{ 
+const newProducts = this.state.products.filter(p => p.id !== productId);
+this.setState ({products : newProducts});
+}
+
+handleReset = ()  => {
+const newProducts = this.state.products.map( p =>{ 
+   p.count = 0 ;
+   return p;
+ } ) 
+this.setState({products : newProducts});
+}
+}
+ 
+
+ export default App;
+
+//function App (){
+    //return (
+               //     <>
+               //     <Navbar></Navbar>
+                //    <Products></Products>
+                //    </>
+              //  );   
+//}
+// export default App;
+
+
 
