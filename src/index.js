dc7bd6cf2f7a379da490265dd63829d618cfb0dc
shahab.zamani <shahab.mirzamani@gmail.com>
2022-02-25 12:37:29 -0500

    add tag app componet to indexjs reactdom render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import App from './app';
 import reactDOM from 'react-dom'; 
 import {render} from "@testing-library/react";
-import Products from './components/functional/products'
+//import Products from './components/functional/products'
 //import Products from './components/products'
 
 //import Product from './components/product'
 //import Product from './components/functional/product'
 
 // const element = <h1>Products Program</h1>
-reactDOM.render(<Products/>,document.getElementById('root'));
+reactDOM.render(<App/>,document.getElementById('root'));
 
