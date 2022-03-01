c1668576b9f1a1f7a120ef2f26821f8800b6daf0
shahab.zamani <shahab.mirzamani@gmail.com>
2022-03-01 15:57:20 -0500

    context type context inchildren-nothis usecontext

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Product from './product';
+import ProductContext from './../Context/products';
 
 class Products extends Component {
+    static contextType = ProductContext;
     
 
     render() { 
         return ( 
         <>
-        <button onClick = { this.props.onReset} className = 'btn btn-primary' >Reset</button>
-        {this.props.products.map((p,index) => (
-         <Product onIncrement = {this.props.onIncrement} onDecrement = {this.props.onDecrement}  onDelete= {this.props.onDelete} id= {p.id}  key ={index} count = {p.count} productName = {p.productName} > Product counter </Product>
+        <button onClick = { this.context.onReset} className = 'btn btn-primary' >Reset</button>
+        {this.context.products.map((p,index) => (
+         <Product /*onIncrement = {this.context.onIncrement} onDecrement = {this.context.onDecrement}  onDelete= {this.context.onDelete} */ id= {p.id}  key ={index} count = {p.count} productName = {p.productName} > Product counter </Product>
          ))}
 
         </>
